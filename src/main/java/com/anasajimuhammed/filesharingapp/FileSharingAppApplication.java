f319d0f57411c569338c60f7698c31e30fde6fc0
anas-aji-muhammed <anas.ajeeb2@gmail.com>
2024-01-15 14:56:24 +0000

    make it better

---
+++ src/main/java/com/anasajimuhammed/filesharingapp/FileSharingAppApplication.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.filesharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSharingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anasajimuhammed/filesharingapp/FileSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.filesharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
